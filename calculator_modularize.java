a3962d6596dbd97529455749b75584542ae6f5d8
g17938oy <g17938oy@gm.tsuda.ac.jp>
2019-06-10 15:29:42 +0900

    エラー処理変更

--- calculator_modularize.java
+++ calculator_modularize.java
@@ -2,9 +2,8 @@
 import java.util.*;
 public class calculator_modularize {
 
-
     static ArrayList<Token> tokens = new ArrayList<Token>();
-	    
+    
     private static int readNumber(String line, int index) {
 	double number = 0;
 	while (index < line.length() && Character.isDigit(line.charAt(index))) {
@@ -24,10 +23,18 @@ public class calculator_modularize {
 	return index;
     }
     private static int readPlus(String line, int index) {
+	if(! Character.isDigit(line.charAt(index - 1))) {
+	    System.out.println("Double symbol is invail: " +line.charAt(index - 1) + ", " + line.charAt(index));
+	    System.exit(1);//また入力をうけつけたい
+	}
 	tokens.add(new Token("PLUS"));
 	return index + 1;
     }
     private static int readMinus(String line, int index) {
+	if(! Character.isDigit(line.charAt(index - 1))) {
+	    System.out.println("Double symbol is invail: " +line.charAt(index - 1) + ", " + line.charAt(index));
+	    System.exit(1);//また入力をうけつけたい
+	}
 	tokens.add(new Token("MINUS"));
 	return index + 1;
     }
@@ -42,11 +49,10 @@ public class calculator_modularize {
 		index = readMinus(line, index);
 	    } else {
 		System.out.println("Invalid character found: " + line.charAt(index));
-		break;
-		//本当はans = を出さず処理を中断したい。
+		System.exit(1);//また入力をうけつけたい
 	    }
 	}
-				
+			
     }
     private static double evaluate() {
 	double answer = 0;	
@@ -61,16 +67,15 @@ public class calculator_modularize {
 		    answer -= tokens.get(index).number;
 		} else {
 		    System.out.println("Invalid syntax");
-		    break;
-		    //本当はans = を出さず処理を中断したい。
+		    System.exit(1);//また入力をうけつけたい
 		}
 	    }
 	    index++;
 	}
-	return answer;
+	    return answer;
     }
-				
-	    
+			
+    
     private void test(String s) {
 	//eval関数をどう実装したらいいのかわからない。
     }
@@ -80,17 +85,17 @@ public class calculator_modularize {
 	test("1.0+2.1-3");
 	System.out.println("==== Test finished! ====");
     }
-	    
+    
     public static void main(String[] args) {
 	//runTest();
 	Scanner sc = new Scanner(System.in);
 	while(true) {
+	    tokens.clear();
 	    System.out.print("> ");
 	    String line = sc.next();
 	    tokenize(line);
 	    double answer = evaluate();
 	    System.out.println("answer = " + answer);
-	    tokens.clear();
 	}    
     }
 }
@@ -98,14 +103,14 @@ class Token {
     String type;
     double number = 0;
 
-
     public Token(String type, double number) {
 	this.type = type;
 	this.number = number;
     }
 
-
     public Token(String type) {
 	this.type = type;
     }
 }
+    
+    
